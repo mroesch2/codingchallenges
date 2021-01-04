164ddf3ce0af82122541d9b333aae270a94dcab5
mnroesch42 <mnroesch42@students.tntech.edu>
2021-01-04 13:15:41 -0600

    First commit

---
+++ SquareAreasDiff/SquareAreasDiff.cs
@@ -0,0 +1,35 @@
+/*Imagine a circle and two squares: a smaller and a bigger one. For the smaller one, 
+the circle is a circumcircle and for the bigger one, an incircle.
+Create a function, that takes an integer (radius of the circle) 
+and returns the difference of the areas of the two squares.
+
+Difficulty: Medium 
+*/
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+  
+// namespace declaration 
+namespace CodingChallenge { 
+      
+    // Class declaration 
+    class Challenge 
+    { 
+        
+
+        static public string SquareAreasDiff(int r)
+        {
+        
+        }
+      
+        // Main Method 
+        static void Main(string[] args) 
+        { 
+        
+        }
+    }
+             
+            
+} 

b54e61ffbd39fe5bf841f9c178b8ca7a19eeacfc
mnroesch42 <mnroesch42@students.tntech.edu>
2021-01-04 13:49:45 -0600

    Bare bones created

---
+++ FindVertex/FindVertex.cs
@@ -0,0 +1,36 @@
+/* Every quadratic curve y = a x² + b x + c has a vertex point: 
+the turning point where the curve stops heading down and starts going up.
+
+Given the values a, b and c, you need to return the coordinates of the vertex. 
+Return your answers rounded to 2 decimal places.
+
+Difficulty: Hard
+*/
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+  
+// namespace declaration 
+namespace CodingChallenge { 
+      
+    // Class declaration 
+    class Challenge 
+    { 
+        
+        //Returning multiple values so store it in a double array
+        static public double[] FindVertex(double a, double b, double c)
+        {
+  
+        }
+      
+        // Main Method 
+        static void Main(string[] args) 
+        { 
+           
+        }
+    }
+             
+            
+} 

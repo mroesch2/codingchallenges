eb22cfb45ee04427f0691fbcc436aa5a41a4d439
mnroesch42 <mnroesch42@students.tntech.edu>
2021-01-13 13:21:29 -0600

    File mispelled

--- Uncensor/Uncensor.cs
+++ Uncensor/Uncensor.cs
@@ -17,16 +17,25 @@ namespace CodingChallenge {
     class Challenge 
     { 
         
-        static public stribng Uncensor(string censored)
+        static public string Uncensor(string censored, string vowels)
         {
+           //Convert strings to char arrays to replace each * with matching vowel
+           char[] cen = censored.ToCharArray();
+           char vow = vowels.ToCharArray();
            
+           for (int i = 0; i < censored.Length; i++)
+           {
+
+           }
 
         }
       
         // Main Method 
         static void Main(string[] args) 
         { 
-          
+          string result = Uncensor("Wh*r* d*d my v*w*ls g*?", "eeioeo");
+
+          Console.WriteLine(result);
         }
     }
              

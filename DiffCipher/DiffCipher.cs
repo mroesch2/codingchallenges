4495b6467c16151fe6260f25028d4dd8a00991fc
mnroesch42 <mnroesch42@students.tntech.edu>
2021-01-13 14:16:49 -0600

    Bare bones code

---
+++ DiffCipher/DiffCipher.cs
@@ -0,0 +1,40 @@
+/* It's time to send and receive secret messages.
+Create two functions that take a string and an array and returns a coded or decoded message.
+The first letter of the string, or the first element of the array represents the
+ Character Code of that letter. The next elements are the differences between the characters: 
+ e.g. A +3 --> C or z -1 --> y.
+
+Difficulty: Hard
+*/
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+  
+// namespace declaration 
+namespace CodingChallenge { 
+      
+    // Class declaration 
+    class Challenge 
+    { 
+        
+        static public string Encode(string message)
+        {
+          
+        }
+
+         static public string Decode(string message)
+        {
+          
+        }
+      
+        // Main Method 
+        static void Main(string[] args) 
+        { 
+          
+        }
+    }
+             
+            
+} 

953b73e6b378b0308e1599ce796a3e05375da327
mnroesch42 <mnroesch42@students.tntech.edu>
2021-01-13 14:59:39 -0600

    Encrypt completed. Tests passed

--- DiffCipher/DiffCipher.cs
+++ DiffCipher/DiffCipher.cs
@@ -10,6 +10,7 @@ Difficulty: Hard
 using System;
 using System.Linq;
 using System.Collections.Generic;
+using System.Text;
 
   
 // namespace declaration 
@@ -19,20 +20,53 @@ namespace CodingChallenge {
     class Challenge 
     { 
         
-        static public string Encode(string message)
+        static public string Encrypt(string message)
         {
-          
+            int n;
+            //string answer = " ";
+            char[] cArr = message.ToCharArray();
+            int temp;
+            int diff;
+
+           //Get the starting number
+            n = Convert.ToInt32(cArr[0]);
+
+            //Convert to a string and add to answer
+            string answer = n.ToString() + " ";
+
+            
+            for (int i = 1; i < cArr.Length; i++)
+            {
+                //store next value in a temp variable
+                temp = Convert.ToInt32(cArr[i]);
+                
+                //find the difference between the current and previous number
+                diff = temp - n;
+    
+
+                //store the difference in answer
+                answer += diff.ToString() + " ";
+
+                //Update n to the current value 
+                n = cArr[i];
+            }
+
+            return answer;
         }
 
-         static public string Decode(string message)
+         /*static public string Decrypt(int[] message)
         {
           
-        }
+        }*/
       
         // Main Method 
         static void Main(string[] args) 
         { 
-          
+          string result = Encrypt("Hello");
+          //Decrypt([ 72, 33, -73, 84, -12, -3, 13, -13, -68 ])
+          //string result = Encrypt("Sunshine");
+
+          Console.WriteLine(result);
         }
     }
              

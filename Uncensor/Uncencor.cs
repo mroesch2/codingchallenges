e6386678f33d2033b597f75d7b6668c0615b69af
mnroesch42 <mnroesch42@students.tntech.edu>
2021-01-13 13:10:03 -0600

    Bare bones code

---
+++ Uncensor/Uncencor.cs
@@ -0,0 +1,34 @@
+/* Someone has attempted to censor my strings by replacing every vowel with a *, l*k* th*s. 
+Luckily, I've been able to find the vowels that were removed.
+Given a censored string and a string of the censored vowels, return the original uncensored string.
+
+Difficulty: Hard
+*/
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+
+  
+// namespace declaration 
+namespace CodingChallenge { 
+      
+    // Class declaration 
+    class Challenge 
+    { 
+        
+        static public stribng Uncensor(string censored)
+        {
+           
+
+        }
+      
+        // Main Method 
+        static void Main(string[] args) 
+        { 
+          
+        }
+    }
+             
+            
+} 

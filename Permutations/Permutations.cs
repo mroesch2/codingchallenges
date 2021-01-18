88aededf51ba7702a152bc70212ec7a82427267a
mnroesch42 <mnroesch42@students.tntech.edu>
2021-01-18 14:43:35 -0600

    Bare bones program created

---
+++ Permutations/Permutations.cs
@@ -0,0 +1,47 @@
+/* Create a function to generate all string character permutations.
+Sort your results in alphabetical order before returning them.
+The result should not contain any duplicates.
+
+Difficulty: Very Hard
+*/
+
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+  
+// namespace declaration 
+namespace CodingChallenge { 
+      
+    // Class declaration 
+    class Challenge 
+    { 
+        
+        static public string[] Permutations(string s)
+        {
+           
+        }
+
+         
+      
+        // Main Method 
+        static void Main(string[] args) 
+        { 
+            
+            //Permutations("AB");
+
+            //Permutations("CD");
+
+            //Permutations("EF");
+
+            //Permutations("NOT");
+
+            //Permutations("RAM");
+
+            //Permutations("YAW");
+        }
+    }
+             
+            
+} 
